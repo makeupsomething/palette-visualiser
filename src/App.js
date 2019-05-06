8f6383a59cd249767e3f12e7b8f8adf0d14e960c
daryl <darylcoleoffice@gmail.com>
2019-05-06 22:50:28 +0900

    🎨 Add color picker

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ const App = () => {
 		<Fragment>
 			<Normalize />
 			<InputGroup baseColor="#3183c8" />
+			<InputGroup baseColor="#b8c4ce" />
+			<InputGroup baseColor="#3daea3" />
+			<InputGroup baseColor="#38c171" />
+			<InputGroup baseColor="#f4ca64" />
+			<InputGroup baseColor="#dc2f30" />
 		</Fragment>
 	)
 }
--- src/components/InputGroup.js
+++ src/components/InputGroup.js
@@ -1,66 +1,43 @@
 import React, { Fragment } from 'react'
-import { lightenDarkenColor } from '../utils'
+import styled from 'styled-components'
+import Values from 'values.js'
+import { ChromePicker } from 'react-color'
 import MiniInterface from './MiniInterface'
 
-const generateShades = (baseColor) => {
-	return [
-		lightenDarkenColor(baseColor, -30),
-		lightenDarkenColor(baseColor, -30),
-		lightenDarkenColor(baseColor, -20),
-		baseColor,
-		lightenDarkenColor(baseColor, 20),
-		lightenDarkenColor(baseColor, 30),
-		lightenDarkenColor(baseColor, 40),
-	]
-}
+const Swatch = styled.div`
+	display: inline-block;
+	width: 25px;
+	height: 25px;
+	background-color: ${(props) => props.backgroundColor || 'palevioletred'};
+`
 
-const Input = (props) => {
-	const { index, colors, setColors } = props
-	function updateColor(color) {
-		const newColors = colors.map((c, i) => {
-			if (index !== i) {
-				return c
-			}
-			return color
-		})
-		setColors(newColors)
-	}
-	return (
-		<input
-			type="color"
-			value={colors[index]}
-			onChange={(e) => updateColor(e.target.value)}
-		/>
-	)
+const generateShades = (baseColor) => {
+	const color = new Values(baseColor).all(20)
+	return color.map((c) => {
+		return `#${c.hex}`
+	})
 }
 
 const InputGroup = (props) => {
 	const { baseColor } = props
-	const [colors, setColors] = React.useState([])
+	const [color, setColor] = React.useState(baseColor)
+	const [colors, setColors] = React.useState(generateShades(baseColor))
 
 	React.useEffect(() => {
 		if (colors.length === 0) {
-			setColors(generateShades(baseColor))
+			setColors(generateShades(color))
 		} else {
-			setColors(generateShades(colors[3]))
+			setColors(generateShades(color))
 		}
-	}, [colors])
+	}, [color])
 
 	return (
 		<Fragment>
-			{colors.length > 0
-				? colors.map((color, index) => {
-						return (
-							<Input
-								index={index}
-								colors={colors}
-								setColors={setColors}
-								key={`colors-${index}`}
-							/>
-						)
-				  })
-				: null}
-			<MiniInterface colors={colors} />
+			<ChromePicker color={color} onChange={(color) => setColor(color.hex)} />
+			{colors.map((c) => {
+				return <Swatch backgroundColor={c} key={c} />
+			})}
+			{/* <MiniInterface colors={colors} /> */}
 		</Fragment>
 	)
 }
